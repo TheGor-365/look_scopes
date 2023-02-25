a61b29c0555791ec3c6ded025c3cfef85e152d1f
Gor <61106836+TheGor-365@users.noreply.github.com>
2023-02-25 21:16:20 +0300

    Add scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  scope :get_top_3_posts, -> { order( views: :desc ).limit(3) }
+  scope :short_reads, -> { where("LENGTH(body) < 100") }
+  scope :where_title_contains, -> (title) { where("title LIKE ?", "%#{title}%") }
 end
